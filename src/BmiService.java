6b51206538d92ea5a342211daf9f6ffa55257754
kukh29 <kukh29@gmail.com>
2022-02-13 17:17:58 +0300

    v.2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public double calculate(double weight, double growth) {
 
-        double BMI = weight / (growth * growth / 100 / 100); // индекс массы тела кг/м2
-        return BMI;
+        double bmi = weight / (growth * growth / 100 / 100); // индекс массы тела кг/м2
+        return bmi;
     }
 }
